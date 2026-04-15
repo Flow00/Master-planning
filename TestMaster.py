072c5fa28d0fec83dc4f4c32eb0335dc8f5e018d
Florian <131906060+Flow00@users.noreply.github.com>
2026-04-15 19:07:30 +0200

    Update TestMaster.py

--- TestMaster.py
+++ TestMaster.py
@@ -289,7 +289,7 @@ def get_purchase_lines(uid, models, project_name):
         formatted.append({
             "PO": po_name,
             "Buyer": buyer_map.get(po_id, "Unknown"),
-            "Description": short_desc(l["name"],30),
+            "Description": short_desc(l["name"],50),
             "Ordered": qty_ordered,
             "Received": qty_received,
             "Planned Date": date_planned,
